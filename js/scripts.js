44d751a366cb14ef2b7b1d10469ea89598b0a7da
Bryon Burke <bryonparis@gmail.com>
2019-10-25 10:11:13 -0700

    continue form work

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,63 @@ $(document).ready(function(){
   $("form#survey").submit(function(event) {
     event.preventDefault();
 
-    var userMarvel = $("input:radio[name=marvel]:checked").val();
+    var userGender = $("input:radio[name=gender]:checked").val();
     var userAge = parseInt($("#age").val());
     var userRick = parseInt($("#rickRange").val());
 
-    console.log(userMarvel)
+    console.log(userGender)
     console.log(userAge)
     console.log(userRick)
 
-    if (userMarvel === "ironMan") {
+    if (userGender === "female") {
 
+      >alert("female");
 
-
-      // if (userAge >= 40 && userRick <5) {
-      //   $(".result").text("option1");
-      // }
-      // else if (userAge >= 40 && userRick >=5) {
-      //   $(".result").text("option2");
-      // }
-      // else if (userAge >= 25 && userRick <5) {
-      //   $(".result").text("option3");
-      // }
-      // else if (userAge >= 25 && userRick >=5) {
-      //   $(".result").text("option4");
-      // }
-      // else if (userAge >= 18 && userRick <5) {
-      //   $(".result").text("option5");
-      // }
-      // else if (userAge >= 18 && userRick >=5) {
-      //   $(".result").text("option6");
-      // }
+      if (userAge >= 40 && userRick <5) {
+        $(".result").text("option1");
+      }
+      else if (userAge >= 40 && userRick >=5) {
+        $(".result").text("option2");
+      }
+      else if (userAge >= 25 && userRick <5) {
+        $(".result").text("option3");
+      }
+      else if (userAge >= 25 && userRick >=5) {
+        $(".result").text("option4");
+      }
+      else if (userAge >= 18 && userRick <5) {
+        $(".result").text("option5");
+      }
+      else if (userAge >= 18 && userRick >=5) {
+        $(".result").text("option6");
+      }
     }
+
+
+    else if (userGender === "male") {
+
+      if (userAge >= 40 && userRick <5) {
+        $(".result").text("option7");
+      }
+      else if (userAge >= 40 && userRick >=5) {
+        $(".result").text("option8");
+      }
+      else if (userAge >= 25 && userRick <5) {
+        $(".result").text("option9");
+      }
+      else if (userAge >= 25 && userRick >=5) {
+        $(".result").text("option10");
+      }
+      else if (userAge >= 18 && userRick <5) {
+        $(".result").text("option11");
+      }
+      else if (userAge >= 18 && userRick >=5) {
+        $(".result").text("option12");
+      }
+    }
+
+    else {
+      $(".result").text("tough luck");
+    }
+  });
+});
