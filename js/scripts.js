3067d1da201d03d70b57fa0f1de641030309624a
Bryon Burke <bryonparis@gmail.com>
2019-10-25 13:19:29 -0700

    troubleshooting

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,65 @@
+$(document).ready(function(){
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
 
+    // var userGender = $("input:radio[name=gender]:checked").val();
+    // var userAge = parseInt($("#age").val());
+    var userRick = parseInt($("#rickRange").val());
+    var userDay = parseInt($("#dayRange").val());
+
+    var total = userRick + userDay;
+
+    // console.log(userGender)
+    // console.log(userAge)
+    // console.log(userRick)
+    //
+    // if (userGender === "female") {
+    //
+    //   if (userDay  && userRick <5) {
+    //     $(".result").text("option1");
+    //   }
+    //   else if (userAge >= 40 && userRick >=5) {
+    //     $(".result").text("option2");
+    //   }
+    //   else if (userAge >= 25 && userRick <5) {
+    //     $(".result").text("option3");
+    //   }
+    //   else if (userAge >= 25 && userRick >=5) {
+    //     $(".result").text("option4");
+    //   }
+    //   else if (userAge >= 18 && userRick <5) {
+    //     $(".result").text("option5");
+    //   }
+    //   else if (userAge >= 18 && userRick >=5) {
+    //     $(".result").text("option6");
+    //   }
+    // }
+    //
+    //
+    // else if (userGender === "male") {
+    //
+    //   if (userAge >= 40 && userRick <5) {
+    //     $(".result").text("option7");
+    //   }
+    //   else if (userAge >= 40 && userRick >=5) {
+    //     $(".result").text("option8");
+    //   }
+    //   else if (userAge >= 25 && userRick <5) {
+    //     $(".result").text("option9");
+    //   }
+    //   else if (userAge >= 25 && userRick >=5) {
+    //     $(".result").text("option10");
+    //   }
+    //   else if (userAge >= 18 && userRick <5) {
+    //     $(".result").text("option11");
+    //   }
+    //   else if (userAge >= 18 && userRick >=5) {
+    //     $(".result").text("option12");
+    //   }
+    // }
+    //
+    // else {
+    //   $(".result").text("tough luck");
+    // }
+  });
+});
